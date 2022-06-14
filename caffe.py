c70673ee6b447752c05f07b6a67c4ef2624ac497
Abraham García Del Corral <42215202+AbrahamGarcia22@users.noreply.github.com>
2022-06-13 20:44:13 -0500

    Add files via upload

---
+++ caffe.py
@@ -0,0 +1,93 @@
+import cv2
+import time
+import numpy as np
+
+cap = cv2.VideoCapture('input/video_1.mp4')
+
+# get the video frames' width and height for proper saving of videos
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# create the `VideoWriter()` object
+out = cv2.VideoWriter('output/video_result_1.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
+
+with open('files/classes_coco_cafe.txt', 'r') as f:
+    class_names = f.read().split('\n')
+
+# get a different color array for each of the classes
+COLORS = np.random.uniform(0, 255, size=(len(class_names), 3))
+
+# load the DNN model
+caffe_model = cv2.dnn.readNetFromCaffe('files/deploy.prototxt', 'files/VGG_coco_SSD_300x300_iter_400000.caffemodel')
+
+def preprocessing(image):
+    R,G,B = cv2.split(image)
+    eq_R = cv2.equalizeHist(R)
+    eq_G = cv2.equalizeHist(G)
+    eq_B = cv2.equalizeHist(B)
+    image= cv2.merge([eq_R, eq_G, eq_B])
+    image = cv2.GaussianBlur(image, (3,3), 0)
+    return image
+    
+def detect(image):
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = preprocessing(image)
+    image_height, image_width, _ = image.shape
+
+    blob = cv2.dnn.blobFromImage(image=image, size=(300, 300), mean=(104, 117, 123))
+    caffe_model.setInput(blob)
+
+    output = caffe_model.forward()
+    image_copy = np.copy(image)
+    # print(output[0,0,:])
+    # return
+    # loop over each of the layer outputs
+    # print(output)
+    #print(layerInputs)
+    for detection in output[0, 0, :, :]:
+        confidence = detection[2]
+        if confidence > .5:
+            
+            # get the class id
+            #print(confidence)
+            class_id = detection[1]
+            final_prob = confidence * 100.
+            # map the class id to the class
+            class_name = class_names[int(class_id)-1]
+            color = COLORS[int(class_id)]
+            # get the bounding box coordinates
+            box_x = detection[3] * image_width
+            box_y = detection[4] * image_height
+            # get the bounding box width and height
+            box_width = detection[5] * image_width
+            box_height = detection[6] * image_height
+            text = f"{class_name}, {final_prob:.3f}"
+            cv2.rectangle(image_copy, (int(box_x), int(box_y)), (int(box_width), int(box_height)), color, thickness=2)
+        # put the FPS text on top of the frame
+            cv2.putText(image_copy, text, (int(box_x), int(box_y - 5)), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+    return image_copy
+# image = cv2.imread('../../Notebooks/images/pedestrian_bike.jpeg')
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# res = detect(image)
+# cv2.imshow("o",res)
+# key = cv2.waitKey(30000)#pauses for 3 seconds before fetching next image
+# if key == 27:#if ESC is pressed, exit loop
+#     cv2.destroyAllWindows()
+# def prepocess(img):
+#     img = cv2.eq
+    
+#detect objects in each frame of the video
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        image = frame
+        p = detect(image)
+        cv2.imshow('image', p)
+        out.write(p)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ yolo.py
@@ -0,0 +1,117 @@
+import cv2
+import time
+import numpy as np
+
+cap = cv2.VideoCapture('input/video_3.mp4')
+
+# get the video frames' width and height for proper saving of videos
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+# create the `VideoWriter()` object
+out = cv2.VideoWriter('output/video_result_3.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
+
+with open('files/classes_coco_cafe.txt', 'r') as f:
+    class_names = f.read().split('\n')
+
+# get a different color array for each of the classes
+COLORS = np.random.uniform(0, 255, size=(len(class_names), 3))
+
+# load the DNN model
+yolo_model = cv2.dnn.readNetFromDarknet('files/yolov3.cfg', 'files/yolov3.weights')
+
+ln = yolo_model.getLayerNames()
+ln = [ln[i-1] for i in yolo_model.getUnconnectedOutLayers()]
+def preprocessing(image):
+    R,G,B = cv2.split(image)
+    eq_R = cv2.equalizeHist(R)
+    eq_G = cv2.equalizeHist(G)
+    eq_B = cv2.equalizeHist(B)
+    image= cv2.merge([eq_R, eq_G, eq_B])
+    image = cv2.GaussianBlur(image, (3,3), 0)
+    return image
+    
+def detect(image):
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    image = preprocessing(image)
+    image_height, image_width, _ = image.shape
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), crop=False)
+    yolo_model.setInput(blob)
+
+    layerOutputs = yolo_model.forward(ln)
+    boxes = []
+    confidences = []
+    classIDs = []
+    percentages = []
+
+    image_copy = np.copy(image)
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+ 
+            # map the max confidence to the class label names
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > 0.6:
+                # probs = np.exp(classID) / np.sum(np.exp(classID))
+                final_prob = confidence * 100.
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([image_width, image_height,image_width, image_height])
+                (centerX, centerY, width, height) = box.astype("int")
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+                percentages.append(final_prob)
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            # draw a bounding box rectangle and label on the image
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image_copy, (x, y), (x + w, y + h), color, 2)
+            out_name = class_names[classIDs[i]]
+            percent = percentages[i]
+            text = f"{out_name}, {percent:.3f}"
+            # text = f"{class_names[classIDs[i]]}"
+            cv2.putText(image_copy, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    return image_copy
+# def prepocess(img):
+#     img = cv2.eq
+    
+# detect objects in each frame of the video
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        image = frame
+        p = detect(image)
+        cv2.imshow('image', p)
+        out.write(p)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
